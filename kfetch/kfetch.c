c98b9bd3b8f3a78c9dc056e93bf897e71321074e
Rafael <rafael.mazolinif@gmail.com>
2025-05-14 20:08:59 -0300

    may return hostname string

---
+++ kfetch/kfetch.c
@@ -0,0 +1,126 @@
+#include "chardev.h"
+
+#define DEVICE_NAME "chardev" /* Dev name as it appears in /proc/devices */
+#define BUF_LEN 80            /* Max length of the message from the device */
+
+/* Global variables are declared as static, so are global within the file. */
+
+static int major; /* major number assigned to our device driver */
+
+enum
+{
+    CDEV_NOT_USED,
+    CDEV_EXCLUSIVE_OPEN,
+};
+
+/* Is device open? Used to prevent multiple access to device */
+static atomic_t already_open = ATOMIC_INIT(CDEV_NOT_USED);
+
+static char msg[BUF_LEN + 1]; /* The msg the device will give when asked */
+
+static struct class *cls;
+
+static struct file_operations chardev_fops = {
+    .read = device_read,
+    .write = device_write,
+    .open = device_open,
+    .release = device_release,
+};
+
+static int __init chardev_init(void)
+{
+    major = register_chrdev(0, DEVICE_NAME, &chardev_fops);
+
+    if (major < 0)
+    {
+        pr_alert("Registering char device failed with %d\n", major);
+        return major;
+    }
+
+    pr_info("I was assigned major number %d.\n", major);
+
+    cls = class_create(DEVICE_NAME);
+    device_create(cls, NULL, MKDEV(major, 0), NULL, DEVICE_NAME);
+
+    pr_info("Device created on /dev/%s\n", DEVICE_NAME);
+
+    return 0;
+}
+
+static void __exit chardev_exit(void)
+{
+    device_destroy(cls, MKDEV(major, 0));
+    class_destroy(cls);
+
+    /* Unregister the device */
+    unregister_chrdev(major, DEVICE_NAME);
+}
+
+
+// Called when a process tries to open the device file
+static int device_open(struct inode *inode, struct file *file)
+{
+    if (atomic_cmpxchg(&already_open, CDEV_NOT_USED, CDEV_EXCLUSIVE_OPEN))
+        return -EBUSY;
+
+    sprintf(msg, "I already told you %d times Hello world!\n", counter++);
+    try_module_get(THIS_MODULE);
+
+    return 0;
+}
+
+/* Called when a process closes the device file. */
+static int device_release(struct inode *inode, struct file *file)
+{
+    /* We're now ready for our next caller */
+    atomic_set(&already_open, CDEV_NOT_USED);
+
+    module_put(THIS_MODULE);
+
+    return 0;
+}
+
+// Called when a process tries to read the device file
+static ssize_t device_read(struct file *filp, 
+                           char __user *buffer,
+                           size_t length,   
+                           loff_t *offset)
+{
+    /* Number of bytes actually written to the buffer */
+    int bytes_read = 0;
+    const char *msg_ptr = msg;
+
+    if (!*(msg_ptr + *offset))
+    {                /* we are at the end of message */
+        *offset = 0; /* reset the offset */
+        return 0;    /* signify end of file */
+    }
+
+    msg_ptr += *offset;
+
+    /* Actually put the data into the buffer */
+    while (length && *msg_ptr)
+    {
+        put_user(*(msg_ptr++), buffer++); //copies the data to user space
+        length--;
+        bytes_read++;
+    }
+
+    *offset += bytes_read;
+
+    /* Most read functions return the number of bytes put into the buffer. */
+    return bytes_read;
+}
+
+// Called when a process writes to dev file
+static ssize_t device_write(struct file *filp, const char __user *buff,
+                            size_t len, loff_t *off)
+{
+    pr_alert("Sorry, this operation is not supported.\n");
+    return -EINVAL;
+}
+
+module_init(chardev_init);
+module_exit(chardev_exit);
+
+MODULE_LICENSE("GPL");
---
+++ kfetch/kfetch.h
@@ -0,0 +1,25 @@
+#ifndef CHARDEV_H
+#define CHARDEV_H
+
+#include <linux/atomic.h>
+#include <linux/cdev.h>
+#include <linux/delay.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kernel.h> 
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/types.h>
+#include <linux/uaccess.h>
+#include <linux/version.h>
+#include <linux/utsname.h>
+
+#include <asm/errno.h>
+
+static int kfetch_open(struct inode *, struct file *);
+static int kfetch_release(struct inode *, struct file *);
+static ssize_t kfetch_read(struct file *, char __user *, size_t, loff_t *);
+static ssize_t kfetch_write(struct file *, const char __user *, size_t, loff_t *);
+
+#endif
